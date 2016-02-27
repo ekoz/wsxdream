cc0421b2fe1fe3a4db7aeb053680b9b05c8d0b20
ekoz <eko.z@hotmail.com>
2016-02-27 14:28:53 +0800

    - bug: CLOSE_WAIT数量计算赋值有误

--- jetty-restart/timer-scan.bat
+++ jetty-restart/timer-scan.bat
@@ -1,6 +1,6 @@
 @echo off 
 rem netstat -an|find "TIME_WAIT"|find "8080" /c
-for /f "delims=" %%a in ('netstat -an^|find "CLOSE_WAIT"^|find "8080" /c') do if not defined str set "str=%%a"
+for /f "delims=" %%a in ('netstat -an^|find "CLOSE_WAIT"^|find "8080" /c') do if not defined str set str=%%a
 if %str% GEQ 50 (
 	echo 当前 CLOSE_WAIT 数量 %str%, 需要重启 Jetty 服务。 %date% %time%
 	call jetty-service.bat
